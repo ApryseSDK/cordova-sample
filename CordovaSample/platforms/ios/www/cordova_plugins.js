b1e02df0b521f76df533a3a5d6cd590571c029f9
James Borthwick <jborthwick@pdftron.com>
2019-04-18 11:57:39 -0700

    Remove plugin reference
    Signed-off-by: James Borthwick <jborthwick@pdftron.com>

--- CordovaSample/platforms/ios/www/cordova_plugins.js
+++ CordovaSample/platforms/ios/www/cordova_plugins.js
@@ -1,16 +1,6 @@
 cordova.define('cordova/plugin_list', function(require, exports, module) {
-  module.exports = [
-    {
-      "id": "pdftron-cordova.PDFTron",
-      "file": "plugins/pdftron-cordova/www/PDFTron.js",
-      "pluginId": "pdftron-cordova",
-      "clobbers": [
-        "PDFTron"
-      ]
-    }
-  ];
+  module.exports = [];
   module.exports.metadata = {
-    "cordova-plugin-whitelist": "1.3.3",
-    "pdftron-cordova": "1.0.0"
+    "cordova-plugin-whitelist": "1.3.3"
   };
 });
