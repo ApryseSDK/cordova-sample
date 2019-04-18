5d06ca579ab20dc09b2e343049f3907729e8a025
sgong-pdftron <sgong@pdftron.com>
2019-04-17 17:17:55 -0700

    update android

--- CordovaSample/platforms/android/app/src/main/java/com/pdftron/cordovasample/MainActivity.java
+++ CordovaSample/platforms/android/app/src/main/java/com/pdftron/cordovasample/MainActivity.java
@@ -22,7 +22,7 @@ package com.pdftron.cordovasample;
 import android.os.Bundle;
 import org.apache.cordova.*;
 
-public class MainActivity extends CordovaActivity
+public class MainActivity extends CordovaAppCompatActivity
 {
     @Override
     public void onCreate(Bundle savedInstanceState)
